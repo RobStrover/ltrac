58276b554ab4ec3589750ba79c7abceaec756676
Rob Strover <Rob Strover>
2017-03-02 14:05:40 +0000

    WIP start building the markup through JQuery fluent for client detials form

--- js/app/clientDetails.js
+++ js/app/clientDetails.js
@@ -1,8 +1,7 @@
 function initClientDetails(job_id) {
-    console.log('here');
     showSpinner();
     // $.ajax({
-    //     type: 'POST',
+    //     type: 'POST'
     //     dataType: "json",
     //     url: "app/ajax_return.php",
     //     data: {
@@ -14,4 +13,28 @@ function initClientDetails(job_id) {
     //     }
     // });
     hideSpinner();
+
+
+        // Proprietor Name Search
+
+    var jobSiteAddressTown = getTag("<input/>", {
+        "id":"jobModal"+jobData.job_id+"JobProprietorName",
+        "class":"form-control jobModalField",
+        "data-dbvar":"job_site_address_town",
+        "disabled":"disabled",
+        "type":"text",
+        "value":jobData.job_site_address_town
+    });
+
+    var jobSiteAddressTownFormGroup = getTag("<div/>", {
+        "class":"form-group col-sm-6"}).append(getTag("<label/>",{
+        'for':"jobModal"+jobData.job_id+"JobSiteAddressTown",
+        "text":"Site Address Town"}),jobSiteAddressTown);
+
+
+
+    $("#jobModal"+jobData.job_id+"JobClientDetailsParent").append(
+
+        );
+
 }
