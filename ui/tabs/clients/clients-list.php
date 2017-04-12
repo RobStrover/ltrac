c5e3d17ac201a45e14e96551d56a45167e107a70
Rob Strover <Rob Strover>
2017-04-12 14:03:38 +0100

    also commit template file.

--- ui/tabs/clients/clients-list.php
+++ ui/tabs/clients/clients-list.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-sm-12 client-tab-search" data-list-type="person" id="client-search-list">
+    <div class="col-sm-12 client-tab-search-list" data-list-type="person" id="client-search-list">
 
     </div>
 </div>
